707522d130318ed7a613ca3e741ce72a573d8f4b
niieeer <clement0201@icloud.com>
2021-12-14 19:56:48 +0100

    "Balise script + Manipulation DOM"

---
+++ index.js
@@ -0,0 +1,5 @@
+const div = document.createElement("div");
+let h1 = document.createElement("h1");
+h1.innerHTML = "Tu tires ou tu pointes ?";
+div.appendChild(h1);
+document.querySelector("body").appendChild(div);
